c5e00f96cca8c77b709aefd37c9979882ace1433
JCiechanowski93 <j.ciechanowski93@gmail.com>
2021-06-23 11:48:39 +0200

    wykop-v2 API

---
+++ client/src/App.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './App.css';
+const logo = require("./logo.svg") as string;
+const Wykop = require('wykop-v2');
+
+/*const wykop = new Wykop({
+  // These are default values.
+  appkey: 'gTt8NvjUNx',
+  secret: 'gvVHpzw1xK',
+  ssl: true,
+  host: 'a2.wykop.pl',
+  userAgent: `wykop-v2-js/dev`,
+  errorTelemetry: true,
+}); */
+
+
+
+
+class App extends Component {
+  state = {
+    data: null
+  };
+
+
+
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+    this.callWykopAPI()
+      .then(res => this.setState({ profileData: res }))
+      .catch(err => console.log(err));
+  }
+  // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  callWykopAPI = async () => {
+    const response = await fetch("https://a2.wykop.pl/Tags/Index/teczowepaski/2/appkey/aNd401dAPp");
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    console.log(body);
+    return body;
+  };
+
+
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header" >
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title" > Welcome to React </h1>
+        </header>
+        <p className="App-intro" > {this.state.data} </p>
+      </div>
+    );
+  }
+}
+
+export default App;
